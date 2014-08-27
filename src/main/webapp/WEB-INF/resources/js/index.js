096f7f6498f3b826741c7257c0b9d619cd192e2f
Evgeniy Baranuk <evgeniy.baranuk@gmail.com>
2014-08-27 19:28:16 +0300

    stfu

--- src/main/webapp/WEB-INF/resources/js/index.js
+++ src/main/webapp/WEB-INF/resources/js/index.js
@@ -3,7 +3,7 @@
  */
 
 $(document).ready(function() {
-    $("#wraper").css("minHeight", $(window).height() - ($("#foot").height())); //todo O_O
+    $("#wraper").css("minHeight", $(window).height()); //todo O_O
 
     getMatches();
 
